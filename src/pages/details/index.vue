69aed3ce5618f9fc02de293a282a1eeca661ac6f
licheng <lic@xspace.gd.cn>
2018-10-19 16:48:31 +0800

    仰視優化

--- src/pages/details/index.vue
+++ src/pages/details/index.vue
@@ -10,7 +10,8 @@
         </div>
         <p><span>[年代]&nbsp;&nbsp;</span>古时候</p>
         <p><span>[文物现状]&nbsp;&nbsp;</span>卖了</p>
-        <p><span>[简介]&nbsp;&nbsp;</span>
+        <p><span>[简介]&nbsp;&nbsp;</span>英雄联盟ID：硕大的根</p>
+        <p class="detailText">
           一年来，以习近平同志为核心的党中央以“不畏浮云遮望眼”的宏阔视野、“乱云飞渡仍从容”的战略定力、
           “千磨万击还坚劲”的意志品质，总揽“四个伟大”，统筹推进“五位一体”总体布局，协调推进“四个全面”战略布局
           ，团结带领全党全军全国各族人民，坚定不移向着“两个一百年”奋斗目标奋力前行 浙江省宁波市人民检察院指控
@@ -62,6 +63,9 @@
     line-height: 60rpx;
     font-size: 36rpx;
     color: #b78f51;
+  }
+  .detailInfo .detailText{
+    color: #7e7e7e;
   }
   .detailInfo span{
     font-weight: 600;
