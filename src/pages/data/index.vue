6febe4ea296c78b0aaa680d19da20cd30e347086
licheng <lic@xspace.gd.cn>
2018-10-19 15:53:41 +0800

    新增頁面

---
+++ src/pages/data/index.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="tab-slider">
+    <div class="tab">
+      <span v-for="(item, index) in items"  @click="toggle(index)"><a v-bind:class="{active: actived == index}">{{item.tab}}</a></span>
+    </div>
+    <div class="tab-content">
+        <tab1 v-if="tab0Control"></tab1>
+        <tab2 v-if="tab1Control"></tab2>
+        <tab3 v-if="tab2Control"></tab3>
+        <tab4 v-if="tab3Control"></tab4>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { mapActions } from 'vuex'
+  import tab1 from '../../components/tab1'
+  import tab2 from '../../components/tab2'
+  import tab3 from '../../components/tab3'
+  import tab4 from '../../components/tab4'
+
+
+  export default {
+    name: 'tabSlider',
+    data (){
+      return {
+        actived: 0,
+        tab0Control:true,
+        tab1Control:false,
+        tab2Control:false,
+        tab3Control:false,
+        items: [{
+          'tab': '今天',
+        },{
+          'tab': '昨天',
+        },{
+          'tab': '一周',
+        },{
+          'tab': '汇总',
+        }]
+      }
+    },
+    components:{
+      tab1,
+      tab2,
+      tab3,
+      tab4
+    },
+    methods: {
+      //v-if控制tab页面的显示与隐藏
+      toggle(index){
+        this.actived = index;
+        if(this.actived === 0){
+            this.tab0Control = true;
+            this.tab1Control = false;
+            this.tab2Control = false;
+            this.tab3Control = false;
+        }else if(this.actived === 1){
+            this.tab0Control = false;
+            this.tab1Control = true;
+            this.tab2Control = false;
+            this.tab3Control = false;
+        }else if(this.actived === 2){
+          this.tab0Control = false;
+          this.tab1Control = false;
+          this.tab2Control = true;
+          this.tab3Control = false;
+        }else if(this.actived === 3){
+          this.tab0Control = false;
+          this.tab1Control = false;
+          this.tab2Control = false;
+          this.tab3Control = true;
+        }
+      }
+
+    }
+  }
+</script>
+
+<style>
+  /**{margin:0 auto;padding:0;font-family:"微软雅黑";}*/
+  .clearboth::after{
+    content:"";
+    display:block;
+    clear:both;
+  }
+  .tab-slider{
+    width:100%;
+    height:100vh;
+    background: #f6f4f3;
+
+  }
+  .tab-slider .tab{
+    display:flex;
+    height:80rpx;
+    line-height:80rpx;
+    border-bottom: 2rpx solid #cbcbca;
+  }
+  .tab-slider .tab span{
+    display:block;
+    width:100%;
+    text-align:center;
+    cursor:default;
+  }
+  .tab-slider .tab span a{
+    display: block;
+    width: 40%;
+    margin: 0 30%;
+    height: 80rpx;
+    font-weight: 500;
+     box-sizing: border-box;
+  }
+  .tab-slider .tab .active {
+    color: #B48A4A;
+    border-bottom: 4rpx solid #B48A4A;
+  }
+  .tab-slider .tab-content{
+    width:100%;
+    overflow:hidden;
+  }
+
+</style>
--- src/pages/me/index.vue
+++ src/pages/me/index.vue
@@ -1,38 +1,162 @@
 <template>
-  <div class="counter-warp">
-    <p>Vuex counter：{{ count }}</p>
-    <p>
-      <button @click="increment">+</button>
-      <button @click="decrement">-</button>
-    </p>
+  <div>
+    <div class="container" v-if="myCover">
+      <img class="bg" src="https://demofile.xspace.gd.cn/wx3Dshow/ui/bg.png" alt="">
+      <div class="decText">
+        <p>即刻开启-3D等你来show</p>
+        <p>让商品展示更多元，精彩</p>
+      </div>
+      <div class="create">我要创建展厅 </div>
+      <div class="noCreate" @click="noCreate">暂不创建</div>
+    </div>
 
-    <a href="/pages/index/main" class="home">去往首页</a>
+    <div class="myRounter" v-if="userInfo">
+      <div class="userinfo">
+        <img class="mebg" src="https://demofile.xspace.gd.cn/wx3Dshow/ui/mebg.png" alt="">
+        <open-data class="infoImg" type="userAvatarUrl"></open-data>
+        <open-data class="userinfo-nickname" type="userNickName"></open-data>
+      </div>
+      <div>
+        <a href="/pages/data/main" class="about">- 我的数据 -</a>
+        <a href="/pages/hall/main" class="collect">- 我的展厅 -</a>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 // Use Vuex
-
 export default {
+  data () {
+    return {
+      myCover:true,
+      userInfo:false
+    }
+  },
   computed: {
 
   },
-  methods: {
-
+  methods:{
+    noCreate(){
+     this.myCover = false;
+      this.userInfo = true;
+    }
   }
+
 }
 </script>
 
 <style>
-.counter-warp {
+.container {
+  position: relative;
+  width: 100%;
+  height:100vh;
+  /*background: url("/static/img/huagongguan@1.5x.png") no-repeat center;*/
+  background-size: 100% 100%;
+}
+.container .bg{
+  width: 100%;
+  position: relative;
+  height: 810rpx;
+}
+.container .decText{
+  position: absolute;
+  top: 42%;
+  left: 0;
+  right: 0;
   text-align: center;
-  margin-top: 100px;
 }
-.home {
-  display: inline-block;
-  margin: 100px auto;
-  padding: 5px 10px;
-  color: blue;
-  border: 1px solid blue;
+.container .decText p:first-child{
+  color: #333;
+  font-weight: 600;
+  font-size: 56rpx;
 }
+.container .decText p:last-child{
+  color: #414141;
+  font-weight: 500;
+  font-size: 40rpx;
+  margin-top: 10rpx;
+}
+  .create{
+    width: 90%;
+    height: 88rpx;
+    line-height: 88rpx;
+    /*padding: 0 5%;*/
+    margin: 0 5%;
+    border: 0!important;
+    background: #c29a5c;
+    color: #fff;
+    border-radius: 0;
+    position: absolute;
+    bottom: 100rpx;
+    font-size: 18px;
+    font-weight: 500;
+    text-align: center;
+  }
+.noCreate{
+  width: 90%;
+  height: 88rpx;
+  line-height: 88rpx;
+  /*padding: 0 5%;*/
+  margin: 0 5%;
+  background: #fff;
+  color: #7e7e7e;
+  border-radius: 0;
+  position: absolute;
+  bottom: 20rpx;
+  font-size: 18px;
+  text-align: center;
+}
+
+
+.myRounter{
+  width: 100%;
+  height: 100vh;
+  background: #f6f4f3;
+}
+
+
+.userinfo {
+  width: 100%;
+  height: 372rpx;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-bottom: 2rpx solid #7e7e7e;
+}
+
+.infoImg {
+  width: 128rpx;
+  height: 128rpx;
+  margin: 50rpx 50rpx 20rpx 50rpx;
+  overflow:hidden;
+  border-radius: 50%;
+  z-index: 22;
+}
+.userinfo-nickname{
+  z-index: 22;
+}
+.mebg{
+  width: 100%;
+  height: 372rpx;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 11;
+  background: #fff;
+  filter: blur(6px);
+}
+.about {
+  margin-top: 40rpx;
+  padding: 30rpx 80rpx;
+  text-align: center;
+  font-size: 36rpx;
+}
+.collect{
+  padding: 30rpx 80rpx;
+  text-align: center;
+  font-size: 36rpx;
+}
+
 </style>
